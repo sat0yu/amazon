7f5532d26b513d7af4bc6dbbc31d03ba15f36a4d
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2013-06-09 22:42:50 +0900

    make kernel.pyx ad cython code

--- kernel.pyx
+++ kernel.pyx
@@ -1,5 +1,6 @@
 #coding: utf-8
 import numpy as np
+cimport numpy as np
 from abc import ABCMeta, abstractmethod
 import sys
 
@@ -10,34 +11,42 @@ class Kernel():
     def val(self, X1, X2): pass
 
     def gram(self, X):
-        N = len(X)
+        cdef N = len(X)
         gm = np.identity(N)
+        cdef i,j
         for i in range(N):
             for j in range(i, N):
                 gm[j][i] = gm[i][j] = self.val(X[i], X[j])
         return gm
 
     def matrix(self, X1, X2):
-        N, M = len(X1), len(X2)
+        cdef int N = len(X1)
+        cdef int M = len(X2)
         mat = np.zeros((N,M))
+        cdef int i,j
         for i in range(N):
             for j in range(M):
                 mat[i][j] = self.val(X1[i], X2[j])
         return mat
 
 class HammingKernel(Kernel):
-    def __init__(self, d=1):
+    def __init__(self, int d=1):
         self.__d = d
 
     def val(self, x, y):
-        return sum([ 1 if xi == yi else 0 for xi,yi in zip(x,y) ])**self.__d
+        cdef int i
+        cdef int N = len(x)
+        cdef int k = 0
+        for i in range(N):
+            k = k + (1 if x[i] == y[i] else 0)
+        return k**self.__d
 
 class GaussKernel(Kernel):
-    def __init__(self, beta):
+    def __init__(self, double beta):
         self.__beta = beta
 
     def val(self, vec1, vec2):
-        dist = np.linalg.norm(vec1-vec2)
+        cdef double dist = np.linalg.norm(vec1-vec2)
         return np.exp(-self.__beta*(dist**2))
 
 def PCA(data, kernel, d):
