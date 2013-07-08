3d0a07b45b964a988ac79e1adec53a78337fbe70
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2013-07-09 06:46:52 +0900

    weighted hammig kernel(d=2) w/ RSOS(rate=1, nSwap=0) w/ RSOS(rate=7, nSwap=1) w/ RSOS(rate=7, nSwap=2) w/ class_weight({0:1.0, 1:0.1})

--- classify.py
+++ classify.py
@@ -9,56 +9,6 @@ pyximport.install(setup_args={'include_dirs':[np.get_include()]}, inplace=True)
 import kernel
 import mlutil
 
-def weak_classifier(args):
-    i, kernel, traindata, evaldata, target = args
-
-    #separate ACTION column from other features
-    labels = traindata[:,0]
-    train = traindata[:,1:]
-    answers = evaldata[:,0]
-    test = evaldata[:,1:]
-
-    #precomputing
-    print '[%d] gram matrix processing start (%d,%d)' % (i,len(train),len(train))
-    gram = kernel.gram(train)
-    print '[%d] test matrix processing start (%d,%d)' % (i,len(test),len(train))
-    test_mat = kernel.matrix(test, train)
-    print '[%d] target matrix processing start (%d,%d)' % (i,len(target),len(train))
-    target_mat = kernel.matrix(target, train)
-
-    #train, class_weight set to auto mode
-    clf = svm.SVC(kernel='precomputed', class_weight='auto')
-    clf.fit(gram, labels)
-
-    #evaluate classifier
-    predict = clf.predict(test_mat).astype(np.int)
-
-    ## since presict.shape is equal answers.shape,
-    ## answers indices can be used as predict indices
-    posPredict = predict[answers[:]==1]
-    negPredict = predict[answers[:]==0]
-
-    ## posCorrect(negPredict) is represented on {0, 1}
-    nPosCorrect = float( sum(posPredict) )
-    nNegCorrect = float( len(negPredict) - sum(negPredict) )
-    accP = nPosCorrect / len( answers[answers[:]==1] )
-    accN = nNegCorrect / len( answers[answers[:]==0] )
-
-    ## calc. err as (1 - g)
-    ## g is the geometric mean of accP and accN
-    err = 1 - np.sqrt(accP * accN) if accP*accN < 1. else 0.000001
-    beta = np.sqrt( err / (1 - err) )
-    alpha = np.log( 1 / beta )
-    print "[%d] acc+: %f, acc-: %f, err: %f, beta: %f, alpha: %f" % (i,accP,accN,err,beta,alpha)
-
-    #classify
-    predict = clf.predict(target_mat).astype(np.int)
-
-    #replace labels from 0 to -1 and weighten them
-    weighted_predict = err * ( (2 * predict) - np.ones_like(predict) )
-     
-    return weighted_predict
-
 def execute():
     #read data
     traindata = np.loadtxt(open("rawdata/train.csv", "rb"), dtype=np.int, delimiter=',', skiprows=1)
@@ -66,35 +16,41 @@ def execute():
     print 'train data: %d' % len(traindata)
     print 'test data: %d' % len(testdata)
 
-    #separate id column from other features
-    ids = testdata[:,0]
-    test = testdata[:,1:]
-
     #instantiate kernel
     uniq = np.array([7519, 4244, 129, 178, 450, 344, 2359, 68, 344], dtype=np.float)
     whk = kernel.WeightendHammingKernel(uniq/max(uniq), 2)
 
     #imbalanced data processing
-    pos = traindata[traindata[:,0]==1,:]
-    neg = traindata[traindata[:,0]==0,:]
-    print 'positive data: %d' % len(pos)
-    print 'negative data: %d' % len(neg)
-    
-    ## DUS
-    trainset = mlutil.dividingUnderSampling(pos, neg, ratio=2)
-    print 'given %d trainset' % len(trainset)
-    print 'each traindata of trainset: ', trainset[0].shape
+    posdata = traindata[traindata[:,0]==1,:]
+    negdata = traindata[traindata[:,0]==0,:]
+    print 'positive data: %d' % len(posdata)
+    print 'negative data: %d' % len(negdata)
 
-    #multiprocessing
-    pool = Pool(4)
-    argset = [ (i, whk, t, traindata, test) for i,t in enumerate(trainset) ]
-    predictions = pool.map(weak_classifier, argset)
+    #RSOS
+    stripped_negdata = negdata[:,1:]    
+    gained = np.vstack( (stripped_negdata, stripped_negdata) )
+    gained = np.vstack( (gained, mlutil.randomSwapOverSampling(stripped_negdata, 7, 1)) )
+    gained = np.vstack( (gained, mlutil.randomSwapOverSampling(stripped_negdata, 7, 2)) )
+    print 'gained negative data: %d' % len(gained)
+    labels = np.zeros( (len(gained), 1), dtype=np.int)
+    labeled = np.hstack( (labels, gained) )
+    merged = np.vstack( (negdata, labeled) )
+    traindata = np.vstack( (posdata, merged) )
 
-    #calc. predict,
-    #since each prediction is represented based on {-1, 1}
-    #i.e. (alpha * -1) or (alpha * 1)
-    predict = np.sign( sum(predictions) )
-    predict = ( predict + np.ones_like(predict) ) / 2
+    #separate action/id column from other features
+    labels = traindata[:,0]
+    train = traindata[:,1:]
+    ids = testdata[:,0]
+    test = testdata[:,1:]
+
+    #train and classify
+    print 'gram matrix: (%d,%d)' % (train.shape[0],train.shape[0])
+    gram = whk.gram(train)
+    print 'test matrix: (%d,%d)' % (test.shape[0],train.shape[0])
+    mat = whk.matrix(test, train)
+    clf = svm.SVC(kernel='precomputed', class_weight={0:1.0, 1:0.1})
+    clf.fit(gram, labels)
+    predict = clf.predict(mat).astype(np.int)
 
     #output
     output = np.vstack( (ids, predict) ).T
